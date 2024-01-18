a9e4278230e3409d3deba4cc9bfca42ce30d05b6
benab04 <benabrahambiju@gmail.com>
2024-01-19 00:22:17 +0530

    update forms

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -52,7 +52,7 @@ function ContactForm() {
         <input type="submit" value="Submit" className="btn" />
       </form> */}
       <iframe
-        src={`https://docs.google.com/forms/d/e/${process.env.REACT_APP_FORM_ID}/viewform?embedded=true`}
+        src="https://docs.google.com/forms/d/e/1FAIpQLSema4Tvdcc7vR9Y-jpeRRsM-gkw8IYy27RdISF1tFvVRnDZhQ/viewform?embedded=true"
         width="640"
         height="660"
         frameborder="0"
